90c979e212b8965ac2d3018e7f729672812008b8
Joeltt <joelttm15@gmail.com>
2024-09-13 14:44:38 +0100

    feat: Implement two way binding for Person class

--- app/src/main/java/com/example/databindingdemo/datamodel/Person.java
+++ app/src/main/java/com/example/databindingdemo/datamodel/Person.java
@@ -1,13 +1,22 @@
 package com.example.databindingdemo.datamodel;
 
-import androidx.databinding.Bindable;
+import android.widget.EditText;
 
-public class Person {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.InverseBindingAdapter;
+import androidx.databinding.InverseMethod;
+import androidx.databinding.library.baseAdapters.BR;
+
+import com.example.databindingdemo.MainActivity;
+
+public class Person extends BaseObservable {
     private String name;
-    private int age;
+    private Integer age;
     private String email;
 
-    public Person(String name, int age, String email) {
+    public Person(String name, Integer age, String email) {
         this.name = name;
         this.age = age;
         this.email = email;
@@ -19,15 +28,25 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
     @Bindable
-    public int getAge() {
-        return age;
+    public Integer getAge() {
+        try{
+            if (this.age == null) {
+                return null;
+            } else {
+                return age;
+            }
+        }catch(NumberFormatException e) {
+            throw new NumberFormatException("Wrong format input");
+        }
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 
     @Bindable
@@ -37,5 +56,19 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
+//    @BindingAdapter("age")
+//    public void setAgeAdapter(MainActivity view, int age) {
+//        // Important to break potential infinite loops.
+//        if (view.age != this.age) {
+//            this.age = age;
+//        }
+//    }
+//
+//    @InverseBindingAdapter(attribute = "age")
+//    public  int getAge(EditText view) {
+//        return view.getAge();
+//    }
+
 }
